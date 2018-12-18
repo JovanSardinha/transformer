3604993e189acd6e0fdc951e62d10b304d75bdba
JovanSardinha <jovan.sardinha@gmail.com>
2018-12-18 07:43:54 +0000

    cleaned prepro.py file

--- prepro.py
+++ prepro.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #/usr/bin/python2
 '''
-June 2017 by kyubyong park. 
+June 2017 by kyubyong park.
 kbpark.linguist@gmail.com.
 https://www.github.com/kyubyong/transformer
 '''
@@ -16,13 +16,13 @@ from collections import Counter
 
 def make_vocab(fpath, fname):
     '''Constructs vocabulary.
-    
+
     Args:
       fpath: A string. Input file path.
       fname: A string. Output file name.
-    
+
     Writes vocabulary line by line to `preprocessed/fname`
-    '''  
+    '''
     text = codecs.open(fpath, 'r', 'utf-8').read()
     text = regex.sub("[^\s\p{Latin}']", "", text)
     words = text.split()
